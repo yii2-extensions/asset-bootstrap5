47acd375af3841d2c84b102273eb143cfa9c1ea7
Wilmer Arambula <terabytesoftw@gmail.com>
2023-11-17 17:53:15 -0300

    Add `cdn`, `popperjs`.

---
+++ src/BootstrapCdnAsset.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii2\Asset;
+
+use yii\web\AssetBundle;
+
+/**
+ * Twitter Bootstrap 5 CDN CSS bundle.
+ */
+final class BootstrapCdnAsset extends AssetBundle
+{
+    /**
+     * @inheritDoc
+     */
+    public $css = [
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public $cssOptions = [
+        'crossorigin' => 'anonymous',
+        'integrity' => 'sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN',
+        'rel' => 'stylesheet',
+    ];
+}
